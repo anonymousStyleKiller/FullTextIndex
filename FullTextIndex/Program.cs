61dbaaa8d26f5021952478d9cb10aba0d39cd9d4
Anton <gumb1t97@gmail.com>
2022-07-15 01:00:07 +0300

    Preparation

--- FullTextIndex/Program.cs
+++ FullTextIndex/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+
+
+var list = TopicsExtractor.ArticleSet().Take(10000).ToArray();
+
+class TopicsExtractor
+{
+    public static IEnumerable<string> ArticleSet()
+    {
+        return ReadArticleSet("records.csv")
+            .Concat(ReadArticleSet("titles.csv"))
+            .Concat(ReadArticleSet("topics.csv"));
+    }
+
+    private static IEnumerable<string> ReadArticleSet(string fileName)
+    {
+        using (var reader = new CsvHelper.CsvReader(
+                   File.OpenText(Path.Combine(@"C:\Projects\OpenSources\FullTextIndex", fileName)),
+                   CultureInfo.InvariantCulture))
+        {
+            reader.Read();
+            reader.ReadHeader();
+            while (reader.Read())
+            {
+                var content = reader["content"];
+                yield return content;
+            }
+        }
+    }
+}
